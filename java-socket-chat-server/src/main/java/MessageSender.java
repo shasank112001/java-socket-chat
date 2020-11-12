1e81485a321d3b24b5871c4ef65d9508572d1f2a
shasank112001 <shasanksekharpandey@gmail.com>
2020-11-12 17:03:11 +0100

    Added the functionality to sendMessages to clients in MessageSender class

--- java-socket-chat-server/src/main/java/MessageSender.java
+++ java-socket-chat-server/src/main/java/MessageSender.java
@@ -17,7 +17,23 @@ public class MessageSender {
     }
     public void sendMessage(int fromClientId,int toClientId, String message )
     {
-
+        Server.ClientHandler receiver=getClient(toClientId);
+        if(receiver!=null)
+            receiver.out.println("Sender= Client number-"+fromClientId+" message=" +message);
+        else
+        {
+            receiver=getClient(fromClientId);
+            receiver.out.println("ERROR. Client cannot available. Message Cannot be sent.");
+        }
+    }
+    public Server.ClientHandler getClient(int toClientId)
+    {
+        for (Server.ClientHandler client : mainServer.clientList)
+        {
+            if(client.clientId==toClientId)
+                return client;
+        }
+        return null;
     }
 
 }
